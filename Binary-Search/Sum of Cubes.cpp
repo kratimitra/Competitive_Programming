ac31c52d28fe28e39082366baff83ab530c3651a
kratimitra <65777327+kratimitra@users.noreply.github.com>
2021-06-16 14:59:15 +0530

    Create Sum of Cubes.cpp

---
+++ Binary-Search/Sum of Cubes.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define codephillic__ ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+
+typedef long long ll;
+#define Mod (int) 998244353
+long long MOD=1e9+7;
+// Big two primes
+#define X 1001100011100001111ll
+#define all(a) a.begin(),a.end()
+#define for0(i, n) for (int i = 0; i < n; i++)
+#define for1(i, n) for (int i = 1; i <= n; i++)
+#define loop(i,a,b) for (int i = a; i < b; i++)
+#define bloop(i,a,b) for (int i = a ; i>=b;i--)
+
+#define pb push_back
+#define prec(n) fixed<<setprecision(n)
+#define maxpq priority_queue<int>
+#define minpq priority_queue<int, vector<int>, greater<int> >
+#define inf (int)(1e18)
+#define fill__(a, i) memset(a, i, sizeof(a))
+#define vi vector<int>
+#define ff first
+#define ss second
+#define endl "\n"
+#define pi 3.1415926536
+#define pii pair<int, int>
+#define vii vector<pi>
+const int MAXN = (int)((1e5) + 100);
+
+unordered_map<ll,bool> m;
+void cook_here()
+{
+    ll n;
+    cin>>n;
+
+    for(ll i=1;i<=n;i++){
+        ll y = i*i*i;
+        ll left = n-y;
+        if(left<=0) break;
+        if(m[left]) {
+            cout<<"YES\n";
+            return;
+        }
+    }
+
+    cout<<"NO\n";
+    return;
+
+
+
+}
+int32_t main() {
+/*
+#ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+#endif
+*/
+    codephillic__;
+//srand(time(NULL));
+    ll t=1;
+    
+    for(ll i=1;i<=1e4;i++)
+    {
+        m[i*i*i] = true;
+    }
+    cin>>t;
+    while(t--){
+        cook_here();
+    }
+    return 0;
+    
+
+
+}
